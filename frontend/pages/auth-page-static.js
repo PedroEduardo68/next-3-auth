24afb9b02d7456f6027bb12535896cb5c1510697
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2022-10-13 22:19:26 -0300

    static hability

--- frontend/pages/auth-page-static.js
+++ frontend/pages/auth-page-static.js
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react"
+import { authService } from "../src/service/auth/authService"
+
+const useSession = () =>{
+    const [ session, setSession] = useState(null)
+    const [ loading , setLoading] = useState(true)
+    const [ error, setError] = useState(null)
+
+
+    useEffect(()=>{
+        authService.getSession()
+        .then((userSession) =>{
+            setSession(userSession)
+        })
+        .catch((err)=>{
+            setError(err)
+        })
+        .finally(()=>{
+            setLoading(false)
+        })
+    }, [])
+    
+
+    return {
+        data: session,
+        error,
+        loading,
+    }
+}
+
+
+
 const AuthpageStatic = (props) =>{
+    const session = useSession()
     return(
         <>
             <div>
                 <h1> Auth Page Static </h1>
                 <pre>
-                    {JSON.stringify(props, null, 2)}
+                    {JSON.stringify(session, null, 2)}
                 </pre>
             </div>
         </>
